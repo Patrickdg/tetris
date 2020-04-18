91d17f28a4cafb03b790a80a5103af3deb00a532
Patrickdg <deguzman.patrick@outlook.com>
2020-04-18 15:30:00 -0400

    block structure and movement logic

--- main.py
+++ main.py
@@ -1,48 +1,91 @@
 import pygame
 import os 
 import random
+from pygame.time import Clock
 
 # PARAMETERS 
 pygame.init()   
 
-WIN_W = 450
-WIN_H = WIN_W * 2
+WIN_W = 300
+WIN_H = 600
 SCREEN = pygame.display.set_mode([WIN_W, WIN_H])
 pygame.display.set_caption("Tetris")
 
-BLOCKS = ["L", "J", "S", "Z", "SQ", "I", "T"]
-BLOCK_SIZE = int(WIN_W/10)
+SZ = int(WIN_W/10)
+BLOCKS = {"L": {'coords': [[0,SZ], [SZ,SZ], [2*SZ,SZ], [2*SZ,0]], 
+                'color':(255,128,0)},
+          "J": {'coords': [[0,0], [0,SZ], [SZ,SZ], [2*SZ,SZ]], 
+                'color':(0,0,255)},
+          "S": {'coords': [[0,SZ], [SZ,SZ], [SZ,0], [2*SZ,0]], 
+                'color':(0,255,0)},
+          "Z": {'coords': [[0,0], [SZ,0], [SZ,SZ], [2*SZ,SZ]], 
+                'color':(255,0,0)},
+          "SQ":{'coords': [[0,0], [0,SZ], [SZ,0], [SZ,SZ]], 
+                'color':(255,255,0)},
+          "I": {'coords': [[0,0], [SZ,0], [2*SZ,0], [3*SZ,0]], 
+                'color':(0,255,255)},
+          "T": {'coords': [[0,SZ], [SZ,0], [SZ,SZ], [2*SZ,SZ]], 
+                'color':(153,51,255)}
+        }
+
+SZ = int(WIN_W/10)
 
 GRID = []
-for i in range(int(WIN_H/BLOCK_SIZE)):
+for i in range(int(WIN_H/SZ)):
     row = []
-    for j in range(int(WIN_W/BLOCK_SIZE)):
+    for j in range(int(WIN_W/SZ)):
         row.append(0)
     GRID.append(row)
 print(GRID)
 
 # OBJECTS 
 class Block():
+    MID_SCREEN = WIN_W/2
+    DY = SZ
     def __init__(self, block):
         self.block = block
-        self.active = False
+        self.placed = False
         self.x = WIN_W/2
         self.y = WIN_H/4
+        self.coords = BLOCKS[self.block]['coords']
+        self.color = BLOCKS[self.block]['color']
     def rotate(self):
         pass
-    def move(self):
+    def move(self, dir):
+        x_change = 0
+        y_change = 0 
+        if dir == 'left':
+            x_change = -SZ
+        elif dir == 'right':
+            x_change = SZ
+        elif dir == 'up':
+            y_change = -SZ
+        elif dir == 'down':
+            y_change = SZ
+        
+        self.coords = [[x + x_change, y + y_change] for [x,y] in self.coords]
+
+    # TO-DO: Method to trigger transfer self.coords into placed vector for crash detection (other pieces AND bare floor)
+    def place(self):
+        self.placed = True
         pass
+
     def draw(self,screen):
-        screen.blit(self.block, (self.x, self.y))
+        for piece in self.coords:
+            pygame.draw.rect(screen, self.color, 
+                (piece[0]+self.MID_SCREEN-SZ, piece[1]+self.DY, 
+                SZ, SZ)
+            )
+        print(self.coords)
+    
 
 # MAIN LOOP
 def main():
-    active_block = Block(random.choice(BLOCKS))
+    clock = pygame.time.Clock()
+    active_block = Block(random.choice(list(BLOCKS.keys())))
     running = True
     while running:
         SCREEN.fill((0,0,0))
-        pygame.time.delay(200)
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -55,8 +98,13 @@ def main():
                     active_block.move('right')
                 elif event.key == pygame.K_DOWN:
                     active_block.move('down')
+                elif event.key == pygame.K_UP:
+                    active_block.rotate()
+
+        active_block.move('down')
         active_block.draw(SCREEN)
         pygame.display.update()
+        clock.tick(2)
     pygame.quit()
 
 main()
