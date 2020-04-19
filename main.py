76f93deb0932fb65ef3f401637f89a644ffc0664
Patrickdg <deguzman.patrick@outlook.com>
2020-04-19 07:13:57 -0400

    initial framework for side collision flag and minor adjustments

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import pygame
 import os 
 import random
+import pygame
 from pygame.time import Clock
 
 # PARAMETERS 
@@ -30,33 +30,38 @@ BLOCKS = {"L": {'coords': [[0,SZ], [SZ,SZ], [2*SZ,SZ], [2*SZ,0]],
 
 SZ = int(WIN_W/10)
 
-GRID = []
-for i in range(int(WIN_H/SZ)):
-    row = []
-    for j in range(int(WIN_W/SZ)):
-        row.append(0)
-    GRID.append(row)
-print(GRID)
+# Initialize vector to track unavailable coordinates and insert 'ground' in PLACED
+PLACED = []
+for r in range(0,WIN_W, SZ):
+    PLACED.insert(0,[r, WIN_H])
 
 # OBJECTS 
 class Block():
     MID_SCREEN = WIN_W/2
     DY = SZ
+
     def __init__(self, block):
         self.block = block
-        self.placed = False
         self.x = WIN_W/2
         self.y = WIN_H/4
         self.coords = BLOCKS[self.block]['coords']
+        self.placed = False
+        self.side_flag = False
         self.color = BLOCKS[self.block]['color']
+    
+        # Displace block coordinates to spawn zone
+        for n in range(len(self.coords)):
+            self.coords[n] = [self.coords[n][0] + self.MID_SCREEN - SZ, 
+                              self.coords[n][1] + SZ]
+
     def rotate(self):
         pass
     def move(self, dir):
         x_change = 0
         y_change = 0 
-        if dir == 'left':
+        if dir == 'left' and not self.side_flag:
             x_change = -SZ
-        elif dir == 'right':
+        elif dir == 'right' and not self.side_flag:
             x_change = SZ
         elif dir == 'up':
             y_change = -SZ
@@ -65,18 +70,23 @@ class Block():
         
         self.coords = [[x + x_change, y + y_change] for [x,y] in self.coords]
 
-    # TO-DO: Method to trigger transfer self.coords into placed vector for crash detection (other pieces AND bare floor)
+        side_flag = any(0 in coord for coord in self.coords) or any((WIN_W - SZ) in coord for coord in self.coords)
+        self.side_flag = True if side_flag else False
+
+    # TO-DO: trigger transfer self.coords into placed vector for crash detection (other pieces AND bare floor)
     def place(self):
         self.placed = True
-        pass
+        for coord in self.coords: 
+            PLACED.insert(coord)
 
     def draw(self,screen):
         for piece in self.coords:
             pygame.draw.rect(screen, self.color, 
-                (piece[0]+self.MID_SCREEN-SZ, piece[1]+self.DY, 
+                (piece[0], piece[1], 
                 SZ, SZ)
             )
         print(self.coords)
+        print(PLACED)
     
 
 # MAIN LOOP
@@ -101,7 +111,8 @@ def main():
                 elif event.key == pygame.K_UP:
                     active_block.rotate()
 
-        active_block.move('down')
+        if not active_block.placed:
+            active_block.move('down')
         active_block.draw(SCREEN)
         pygame.display.update()
         clock.tick(2)
